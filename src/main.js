d20de996de17502c0f4f10ba42dbebc3bc91f806
Minh Tang Q <minh.tang1@tomosia.com>
2024-04-07 15:43:33 +0700

    Install bootstrap

--- src/main.js
+++ src/main.js
@@ -6,9 +6,13 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js'
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(bootstrap)
 
 app.mount('#app')
